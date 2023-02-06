1968f474ea1ab08bb0d701e5af87541cdf599435
Daniel Espinal <dancancode@protonmail.com>
2023-02-06 10:46:43 -0500

    homepage setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      desiredChainId={ChainId.Goerli}
+      chainRpc={{
+        [ChainId.Goerli]:
+          "https://goerli.infura.io/v3/1ad23bb5016a4630aa6141ed368aa603",
+      }}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
